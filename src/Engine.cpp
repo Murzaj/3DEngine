cc320892a39a64855f520b760979333fbbfafd39
Jacek Jarosz <jacekjarosz55@gmail.com>
2024-11-27 00:53:09 +0100

    update gitignore

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -15,32 +15,31 @@ Engine::Engine() {
 }
 
 void Engine::display() {
-    glClearColor(0.0, 0.0, 0.0, 1.0); // this SETS clear color
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // compile the triangle shader
-    glUseProgram(simpleShaderID);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
 
 
-    // 1st attribute buffer : vertices
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glVertexAttribPointer(
-        0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-        3,                  // size
-        GL_FLOAT,           // type
-        GL_FALSE,           // normalized?
-        0,                  // stride
-        (void*)0            // array buffer offset
-    );
-    
-    // Draw the triangle !
-    glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
-    glDisableVertexAttribArray(0);
-    
+    // Draw a teapot
+    glColor3f(1.0f, 0.0f, 0.0f); // Red color
+    glutSolidTeapot(1.0);
+
+    // Draw a sphere
+    glPushMatrix();
+    glTranslatef(-2.0f, 0.0f, 0.0f);
+    glColor3f(0.0f, 1.0f, 0.0f); // Green color
+    glutSolidSphere(0.75, 50, 50);
+    glPopMatrix();
+
+    // Draw a cube
+    glPushMatrix();
+    glTranslatef(2.0f, 0.0f, 0.0f);
+    glColor3f(0.0f, 0.0f, 1.0f); // Blue color
+    glutSolidCube(1.5);
+    glPopMatrix();
 
     glutSwapBuffers();
 }
+
 
 void Engine::onKeyboard(unsigned char key, int x, int y) {
     if (key == 27) {
@@ -61,7 +60,6 @@ void Engine::mainLoop() {
 
 void Engine::initialize(int* argc, char* argv[]) {
     if (instance != nullptr) {
-        throw std::exception("Already initialized.");
     }
     instance = this;
 
