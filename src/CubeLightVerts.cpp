7ff2696d29ee9057a8938825eb4353b2107e0638
Jacek Jarosz <jacekjarosz55@gmail.com>
2024-12-17 10:55:44 +0100

    add cube light verts

--- src/CubeLightVerts.cpp
+++ src/CubeLightVerts.cpp
@@ -1,5 +1,11 @@
 #include "CubeLightVerts.hpp"
 #include <GL/gl.h>
+#include <limits>
+#include <cstdlib>
+
+float randFloatTemp() {
+  return std::rand() / (float)std::numeric_limits<int>::max();
+}
 
 // TODO
 CubeLightVerts::CubeLightVerts() :
@@ -8,58 +14,43 @@ CubeLightVerts::CubeLightVerts() :
       // FACE DOWN
       1.0f, -1.0f, -1.0f,
       -1.0f, -1.0f, -1.0f,
-      1.0f, -1.0f, 1.0f,
-      -1.0f, -1.0f, 1.0f,
+      -1.0f, -1.0f, 1.0f, 
+      1.0f, -1.0f, 1.0f, 
       // FACE UP
-      -1.0f, 1.0f, 1.0f,
-      1.0f, 1.0f, 1.0f,
       1.0f, 1.0f, -1.0f,
       -1.0f, 1.0f, -1.0f,
+      -1.0f, 1.0f, 1.0f, 
+      1.0f, 1.0f, 1.0f, 
       // FACE FRONT
-      -1.0f, 1.0f, 1.0f,
-      1.0f, 1.0f, 1.0f,
       1.0f, -1.0f, 1.0f,
       -1.0f, -1.0f, 1.0f,
+      -1.0f, 1.0f,  1.0f,
+      1.0f, 1.0f, 1.0f,
       // FACE BACK
-      -1.0f, 1.0f, -1.0f,
-      1.0f, 1.0f, -1.0f,
       1.0f, -1.0f, -1.0f,
       -1.0f, -1.0f, -1.0f,
+      -1.0f, 1.0f,  -1.0f,
+      1.0f, 1.0f, -1.0f,
       // FACE LEFT
-      -1.0f, -1.0f, 1.0f,
-      -1.0f, 1.0f, 1.0f,
-      -1.0f, 1.0f, -1.0f,
+      -1.0f, -1.0f, 1.0f, 
       -1.0f, -1.0f, -1.0f,
+      -1.0f,1.0f, -1.0f,
+      -1.0f,1.0f, 1.0f, 
       // FACE RIGHT
-      1.0f, -1.0f, 1.0f,
-      1.0f, 1.0f, 1.0f,
-      1.0f, 1.0f, -1.0f,
+      1.0f, -1.0f, 1.0f, 
       1.0f, -1.0f, -1.0f,
+      1.0f,1.0f, -1.0f,
+      1.0f,1.0f, 1.0f, 
     }, {
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
-      1.0f, 0.0f, 1.0f,
+      // COLORS
+      randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(),
+      randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(),
+      randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(),
+      randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(),
+      randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(),
+      randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(),
+      randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(),
+      randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(), randFloatTemp(),
     },
     {
       // FACE DOWN
@@ -93,8 +84,26 @@ CubeLightVerts::CubeLightVerts() :
       1.0f, 0.0f, 0.0f,
       1.0f, 0.0f, 0.0f,
       },{
-      
-    }
+      //UP
+      0,1,2,
+      0,2,3,
+
+      //DOWN
+      4,5,6,
+      4,6,7,
+      //FRONT
+      8,9,10,
+      8,10,11,
+      //BACK
+      12,13,14,
+      12,14,15,
+      //LEFT
+      16,17,18,
+      16,18,19,
+      //RIGHT
+      20,21,22,
+      20,22,23,
+      }
     ) {}
 
 
